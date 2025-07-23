870f4551582a069e65a8911ba642c2b58bd27dbb
Thi Nguyễn <nguyenthisc1@gmail.com>
2025-07-23 19:38:01 +0700

    update: auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
-import express from 'express';
-import { register, login, logout,refreshToken, registerValidation, loginValidation } from '../controllers/auth.controller.js';
+import express from "express";
+import { AuthController } from "../controllers/auth.controller.js";
+import { LoginUserDTO, RegisterUserDTO } from "../dtos/user.dto.js";
 
 const router = express.Router();
 
-router.post('/register', registerValidation, register);
-router.post('/login', loginValidation, login);
-router.post('/logout', logout);
-router.post('/refresh', refreshToken);
+router.post("/register", RegisterUserDTO.validationRules(), AuthController.register);
+router.post("/login", LoginUserDTO.validationRules(), AuthController.login);
+router.post("/logout", AuthController.logout);
+router.post("/refresh", AuthController.refreshToken);
 
 export default router;
